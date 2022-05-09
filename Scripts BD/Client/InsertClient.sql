d0961d2865d18fe0a0b36520fe5de92390d4adc5
ikerinho18 <ikerpineirorodriguez@gmail.com>
2022-05-09 18:08:08 +0200

    Scripts actualización

---
+++ Scripts BD/Client/InsertClient.sql
@@ -0,0 +1,22 @@
+insert
+	into
+	client 
+(name,
+	lastname,
+	dni,
+	gender,
+	birthday,
+	registration_date,
+	phone,
+	email,
+	active)
+values (
+'Ramón',
+'Díaz Romero',
+'12345678K',
+'M',
+'1980-10-11',
+CURRENT_DATE,
+'611009900',
+'rdiazromero@gmail.com',
+TRUE);
