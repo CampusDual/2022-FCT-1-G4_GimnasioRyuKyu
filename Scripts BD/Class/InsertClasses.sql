749c89c61d69d65d7766c8cf4db20bb15e0e085d
ikerinho18 <ikerpineirorodriguez@gmail.com>
2022-05-10 16:15:58 +0200

    Scripts de class

---
+++ Scripts BD/Class/InsertClasses.sql
@@ -0,0 +1,16 @@
+insert
+	into
+	class 
+(name,
+	description)
+values (
+'Karate',
+'Clases de karate, la arte marcial proveniente de Okinawa');
+insert
+	into
+	class 
+(name,
+	description)
+values (
+'Natación',
+'Clases de natación para todo tipo de edades y niveles');
