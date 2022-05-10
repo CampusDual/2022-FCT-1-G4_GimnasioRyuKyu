c2633792cda8f8dc8c6d621de6c035b38812e131
hmartinvillar <hmartinvillar@gmail.com>
2022-05-10 13:52:46 +0200

    Dao & service

---
+++ gymBack/gymBack/gymBack-model/src/main/java/org/example/model/core/dao/RoomDao.java
@@ -0,0 +1,16 @@
+package org.example.model.core.dao;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
+
+@Repository("RoomDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/RoomDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class RoomDao extends OntimizeJdbcDaoSupport {
+
+    public static final String ATTR_ID = "ID";
+    public static final String ATTR_NAME = "NAME";
+    public static final String ATTR_DESCRIPTION = "DESCRIPTION";
+}
