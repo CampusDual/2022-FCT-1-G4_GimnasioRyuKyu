8c79b42ee373cc472a087eb50084aee1d2d6200d
ikerinho18 <94103661+ikerinho18@users.noreply.github.com>
2022-05-12 13:56:30 +0200

    Delete Scripts BD directory

--- Scripts BD/Room/CreateRoom.sql
+++
@@ -1,5 +0,0 @@
-create table ROOM ( 
-ID INTEGER generated always as identity,
-NAME varchar(30) not null,
-DESCRIPTION varchar(100)
-);
--- Scripts BD/Room/InsertRoom.sql
+++
@@ -1,4 +0,0 @@
-insert into room ("name", description)
-values (
-'Karate',
-'Sala para encontrar el espíritu mas ninja de cada persona');
