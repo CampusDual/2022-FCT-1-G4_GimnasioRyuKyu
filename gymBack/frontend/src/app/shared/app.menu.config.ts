49eb383ef3c6577a8ed2ea2c110321641a401b04
hmartinvillar <hmartinvillar@gmail.com>
2022-05-24 17:55:09 +0200

    revisión traducción y aspecto

--- gymBack/frontend/src/app/shared/app.menu.config.ts
+++ gymBack/frontend/src/app/shared/app.menu.config.ts
@@ -5,10 +5,10 @@ import {
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'clients', name: 'CLIENTS', icon: 'people', route: '/main/clients' },
-  { id: 'classes', name: 'CLASSES', icon: 'class', route: '/main/classes' },
-  { id: 'assign-classes', name: 'ASSING CLASSES', icon: 'grid_view', route: '/main/assign-classes' },
-  { id: 'assign-clients', name: 'ASSING CLIENTS', icon: 'grid_view', route: '/main/assign-clients' },
-  { id: 'rooms', name: 'ROOMS', icon: 'room', route: '/main/rooms' },
+  { id: 'clients', name: 'clients', icon: 'people', route: '/main/clients' },
+  { id: 'classes', name: 'classes', icon: 'class', route: '/main/classes' },
+  { id: 'assign-classes', name: 'assing classes', icon: 'grid_view', route: '/main/assign-classes' },
+  { id: 'assign-clients', name: 'assing clients', icon: 'grid_view', route: '/main/assign-clients' },
+  { id: 'rooms', name: 'rooms', icon: 'room', route: '/main/rooms' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'logout', confirm: 'yes' }
 ];
