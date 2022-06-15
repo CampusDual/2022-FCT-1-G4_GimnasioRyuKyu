aa37c3357f6f136e9ea1a7c51aac00ff807782be
hmartinvillar <hmartinvillar@gmail.com>
2022-06-15 17:20:12 +0200

    G4-169_login cambios en el service

--- gymBack/fontend-client/src/app/components/users/users.service.ts
+++ gymBack/fontend-client/src/app/components/users/users.service.ts
@@ -9,21 +9,21 @@ import { CookieService } from "ngx-cookie-service";
 export class UsersService {
   constructor(private http: HttpClient, private cookies: CookieService) {}
 
-   protected buildHeaders(): HttpHeaders {
+  protected buildHeaders(): HttpHeaders {
 
-     let headers = new HttpHeaders({
+    let headers = new HttpHeaders({
 
-       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': '*',
 
-       'Content-Type': 'application/json;charset=UTF-8'
+      'Content-Type': 'application/json;charset=UTF-8'
 
-     });
+    });
 
-     const sessionId = this.getToken;
-     headers = headers.append('Authorization', 'Basic ' + sessionId);
-     return headers;
+    const sessionId = this.getToken;
+    headers = headers.append('Authorization', 'Bearer ' + sessionId);
+    return headers;
 
-   }
+  }
 
   login(user: any): Observable<any> {
     const options = {
@@ -82,11 +82,3 @@ export class UsersService {
 //   return this.http.get(this.url + '?columns=id_room_class,date,h_start,h_end,id_monitor,class_name,monitor_name,room_name' , options);
 
 // }
-/*
-public login(username:string, password:string){
-
-  const headers = new HttpHeaders({AUthorization: 'Basic ' + btoa(username + ':' + password)});
-
-  return this.http.get("https://localhost:33333/users/login",{headers,responseType: 'text' as 'json'});
-
-}*/
