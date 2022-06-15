da9b5c98563ed7452fed3f8b14a6804f808011f6
manuelplacesalvarez <mplacesalvarez@danielcastelao.org>
2022-06-15 18:03:07 +0200

    Consola devuelve formulario

--- gymBack/fontend-client/src/app/app.module.ts
+++ gymBack/fontend-client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms"
 
 import { AppComponent } from './app.component';
 
@@ -41,6 +42,7 @@ import { SignupComponent } from './components/signup/signup.component';
     SignupComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     MdbAccordionModule,
--- gymBack/fontend-client/src/app/components/signup/signup.component.ts
+++ gymBack/fontend-client/src/app/components/signup/signup.component.ts
@@ -6,9 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+photo: string;
+name: string;
+lastname:string;
+DNI: string;
+email:string;
+password:string;
+birthdate:string;
+phone:BigInteger;
+genremasc:string;
+genrefem:string;
+
+
 
   constructor() { }
+  signup() {
+    console.log(this.photo);
+    console.log(this.name);
+    console.log(this.lastname);
+    console.log(this.DNI);
+    console.log(this.email);
+    console.log(this.password);
+    console.log(this.birthdate);
+    console.log(this.phone);
+    console.log(this.genremasc);
+    console.log(this.genrefem);
 
+  }
   ngOnInit(): void {
 
 
