c8e2e62c8546fd54cfac7620a88fd139e052faea
Pavaneg <91665233+Pavaneg@users.noreply.github.com>
2022-06-15 16:03:52 +0200

    TestsMerges

--- gymBack/frontend/src/app/app.config.ts
+++ gymBack/frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { SERVICE_CONFIG } from './shared/app.services.config';
 
 export const CONFIG: Config = {
   // The base path of the URL used by app services.
-  apiEndpoint: 'http://207.188.182.136:33333',
+  apiEndpoint: 'http://localhost:33333',
   bundle: {
     path: 'bundle'
   },
