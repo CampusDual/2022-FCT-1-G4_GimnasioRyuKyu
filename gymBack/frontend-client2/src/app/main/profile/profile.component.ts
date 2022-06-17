71fb03c3299a9f52e05b30ee4cac6cf1cf3745af
pedromcarril <pedromc@protonmail.com>
2022-06-17 12:12:26 +0200

    HTML Reserva
    Funcionalidades caídas

--- gymBack/frontend-client2/src/app/main/profile/profile.component.ts
+++ gymBack/frontend-client2/src/app/main/profile/profile.component.ts
@@ -1,24 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 
 
 @Component({
 
   selector: 'app-profile',
-
   templateUrl: './profile.component.html',
-
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  encapsulation: ViewEncapsulation.None
 
 })
 
 export class ProfileComponent implements OnInit {
 
-
-
   constructor() { }
-
-
 
   ngOnInit() {}
 
