bfb43c51bd9a1b89e6e15b50dc16f326d5fe9097
pedromcarril <pedromc@protonmail.com>
2022-06-20 15:35:29 +0200

    Retoque form subs

--- gymBack/frontend/src/app/main/subscriptions/subscriptions-detail/subscriptions-detail.component.ts
+++ gymBack/frontend/src/app/main/subscriptions/subscriptions-detail/subscriptions-detail.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-subscriptions-detail',
   templateUrl: './subscriptions-detail.component.html',
-  styleUrls: ['./subscriptions-detail.component.css']
+  styleUrls: ['./subscriptions-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class SubscriptionsDetailComponent implements OnInit {
 
   constructor() { }
